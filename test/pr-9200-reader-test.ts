f8a476488833681eed205210435ee3c302ae918f
Rafael da Mata Neri <rafael.neri@gmail.com>
2018-07-05 00:57:25 -0300

    new tests

--- test/pr-9200-reader-test.ts
+++ test/pr-9200-reader-test.ts
@@ -14,6 +14,23 @@ describe('Pr9200Reader class test', () => {
         });
         expect(result).to.be.instanceof(Pr9200Reader);
     });
+    it('should be equal', () => {
+        const result = Pr9200Reader.getInstance('/dev/tty.wchusbserial1d1130', {
+            autoOpen: false,
+            baudRate: 115200,
+            dataBits: 8,
+            stopBits: 1,
+            parity: "none"
+        });
+        const result2 = Pr9200Reader.getInstance('/dev/tty.wchusbserial1d1130', {
+            autoOpen: false,
+            baudRate: 115200,
+            dataBits: 8,
+            stopBits: 1,
+            parity: "none"
+        });
+        expect(result).to.deep.equals(result2);
+    });
     it('should be a true', () => {
         let reader = Pr9200Reader.getInstance('/dev/tty.wchusbserial1d1130', {
             autoOpen: false,
